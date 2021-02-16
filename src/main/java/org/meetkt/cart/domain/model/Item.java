69e17dc36905d50340a562430384ede2b154d13b
Ranjit Shinde <ranjitshinde91@gmail.com>
2021-02-16 09:43:37 +0530

    Ranjit | Makes Item class as value object i.e. two items are equal if their they are of same product and equal quantity

--- src/main/java/org/meetkt/cart/domain/model/Item.java
+++ src/main/java/org/meetkt/cart/domain/model/Item.java
@@ -2,6 +2,8 @@ package org.meetkt.cart.domain.model;
 
 import org.meetkt.catalogue.domain.model.Product;
 
+import java.util.Objects;
+
 public class Item {
     private Product product;
     private int quantity;
@@ -9,5 +11,19 @@ public class Item {
     public Item(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return quantity == item.quantity &&
+                Objects.equals(product, item.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, quantity);
     }
 }
