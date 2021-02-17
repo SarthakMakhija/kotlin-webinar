4cc443cb51efdb79f5296613cb088ba6eb16f2dd
AkshayChordiya <akshaychordiya2@gmail.com>
2021-02-17 16:03:24 +0530

    Akshay | Create Display model

---
+++ src/main/java/org/meetkt/display/domain/model/Display.java
@@ -0,0 +1,11 @@
+package org.meetkt.display.domain.model;
+
+import org.meetkt.basket.domain.model.Basket;
+
+public class Display {
+
+    private final Basket basket;
+
+    public Display(Basket basket) { this.basket = basket; }
+
+}
