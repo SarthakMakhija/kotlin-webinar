b04846c004da41da9d47ff35cf3574e737aab201
sarthak.makhija@gmail.com <sarthak.makhija@gmail.com>
2021-03-06 16:56:20 +0530

    Sarthak, Ranjit | Adds ItemUnitTest in Kotlin

---
+++ src/test/kotlin/in/meetkt/basket/domain/model/ItemUnitTest.kt
@@ -0,0 +1,29 @@
+package `in`.meetkt.basket.domain.model
+
+import `in`.meetkt.catalogue.domain.model.ProductId
+import `in`.meetkt.catalogue.fixture.ProductFixture
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ItemUnitTest {
+
+    @Test
+    fun shouldReturnTrueGivenItemIsOfAGivenProduct() {
+        val product = ProductFixture.aProduct().withProductId("001").build()
+        val item = Item(product)
+
+        val containsProduct = item.contains(ProductId("001"))
+
+        assertThat(containsProduct).isTrue
+    }
+
+    @Test
+    fun shouldReturnFalseGivenItemIsNotOfGivenProduct() {
+        val product = ProductFixture.aProduct().withProductId("001").build()
+        val item = Item(product)
+
+        val containsProduct = item.contains(ProductId("002"))
+
+        assertThat(containsProduct).isFalse
+    }
+}
