d8e400200eddfdd1e68381e8ce35fecc034d3558
sarthak.makhija@gmail.com <sarthak.makhija@gmail.com>
2021-03-20 19:18:06 +0530

    Ranjit, Sarthak | Converts solitary unit tests to sociable

--- src/test/java/org/meetkt/catalogue/BarcodeScannerUnitTest.java
+++ src/test/java/org/meetkt/catalogue/BarcodeScannerUnitTest.java
@@ -6,23 +6,22 @@ import org.meetkt.catalogue.domain.model.Product;
 import org.meetkt.catalogue.domain.model.ProductId;
 import org.meetkt.catalogue.exception.NoProductFoundForBarcodeException;
 
-import java.util.Optional;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.meetkt.catalogue.fixture.ProductFixture.aProduct;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class BarcodeScannerUnitTest {
 
     @Test
     void shouldThrowAnExceptionGivenProductDoesNotExistForTheBarcode() {
         String barcode = "no-product-found-for-this-barcode";
-        Catalogue catalogue = mock(Catalogue.class);
-        when(catalogue.productWith(barcode)).thenReturn(Optional.empty());
+        Catalogue catalogue = new Catalogue(Collections.emptyList());
 
         BarcodeScanner barcodeScanner = new BarcodeScanner(catalogue);
+
         assertThrows(NoProductFoundForBarcodeException.class,
                 () -> barcodeScanner.scanOrThrow(barcode)
         );
@@ -31,11 +30,11 @@ class BarcodeScannerUnitTest {
     @Test
     void shouldReturnAProductForABarcode() {
         String barcode = "product-001-barcode";
-        Catalogue catalogue = mock(Catalogue.class);
+        Catalogue catalogue = new Catalogue(List.of(aProduct().withProductId("P-001").withBarcode(barcode).build()));
         BarcodeScanner barcodeScanner = new BarcodeScanner(catalogue);
-        when(catalogue.productWith(barcode)).thenReturn(Optional.of(aProduct().withProductId("P-001").withBarcode(barcode).build()));
 
         Product product = barcodeScanner.scanOrThrow(barcode);
+
         assertThat(product.id()).isEqualTo(new ProductId("P-001"));
     }
 }
